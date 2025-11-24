e9347fd20d8dc5076d4cbee9c858b29d574c2612
Mikhail15011976 <NEBUSHKO.MIKHAIL@GMAIL.COM>
2025-11-25 01:17:05 +0300

    add filter

---
+++ filter/src/Apps/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Portfolio from '../Portfolio/Portfolio.jsx';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="portfolio-container">
+      <h1>My Portfolio</h1>
+      <Portfolio />
+    </div>
+  );
+};
+
+export default App;
